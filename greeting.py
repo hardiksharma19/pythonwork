6dc386b7cc00d3762782c7db7f48b35f6b33b8e5
hardiksharma19 <harsharm05@gmail.com>
2025-09-25 03:13:11 +0530

    Create greeting.py

---
+++ greeting.py
@@ -0,0 +1,8 @@
+# Python program to create a personalized greeting
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+full_name = first_name + " " + last_name
+
+print(f"\nHello, {full_name}! Welcome to Python program.")
